c395408bafa69c7c8693215add412061cd3494e1
meriam <hamdaoui.meriam85@gmail.com>
2022-12-28 15:19:17 +0100

    nav main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import MainNav from "./components/navigation/MainNav";
 
 function App() {
-  return <div className="App">ha3 ha3</div>;
+  return (
+    <div className="App">
+      <MainNav />
+    </div>
+  );
 }
 
 export default App;
--- src/components/navigation/MainNav.jsx
+++ src/components/navigation/MainNav.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const MainNav = () => {
-  return <div>MainNav</div>;
+  return (
+    <div>
+      <img
+        className="alpha-logo"
+        src="https://www.alphapublishing.com/wp-content/themes/eduma/images/logo-sticky.png"
+        alt="alpha-log"
+      />
+    </div>
+  );
 };
 
 export default MainNav;
